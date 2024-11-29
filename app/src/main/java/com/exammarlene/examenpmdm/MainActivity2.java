828065e3728216938df164a53d5d19be4ce5bca1
marlenepaper <marlenepaper@hotmail.com>
2024-11-29 14:13:31 +0100

    barra modificada

--- app/src/main/java/com/exammarlene/examenpmdm/MainActivity2.java
+++ app/src/main/java/com/exammarlene/examenpmdm/MainActivity2.java
@@ -39,40 +39,9 @@ public class MainActivity2 extends AppCompatActivity {
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(paginador);
 
-//        setSupportActionBar(binding.toolbar);
-//        getSupportActionBar().setTitle("");
-
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle("");
 
     }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Infla el archivo de menú (menu_desplegable.xml)
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu_desplegable, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.favStarButton) {
-//            Intent intent = new Intent(Aficiones.this, DetalleFragmentActivity.class);
-//            startActivity(intent);
-//            return true;
-//        } else if (id == R.id.homeButton) {
-//            return false;
-//        } else if (id == R.id.aboutMeButton) {
-//            Intent intent = new Intent(Aficiones.this, SobreMi.class);
-//            startActivity(intent);
-//            return true;
-//        } else if (id == R.id.myWebpage) {
-//            Intent intent = new Intent(Intent.ACTION_VIEW);
-//            intent.setData(Uri.parse("https://marlenepaper.com/"));
-//            startActivity(intent);
-//            return true;
-//        } else {
-//            return super.onOptionsItemSelected(item);
-//        }
-//    }
+
 }
